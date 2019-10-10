76d5a5003f5fe6481fb56747fd31f1e25412a6c7
IfnuAzis <azis.ifnu@gmail.com>
2019-10-10 16:42:37 +0700

    new

--- study/src/pages/Index.vue
+++ study/src/pages/Index.vue
@@ -5,18 +5,39 @@
     <button @click ="clearMessage">Reset</button>
   <H5 v-if="message.legth">{{message}} </H5>
   <H4 v-else>No Message</H4>
+
+    <ul>
+      <li v-for="task in tasks"
+      :key="task.name">
+      <div>{{task.name}}</div>
+      <small>due {{task.dueDate}}
+        at {{task.dueTime}}
+      </small> 
+      </li>
+    </ul>
+
   </q-page>
 </template>
 
 <script>
 export default {
  
-data(){
-  return{
-  message: 'Masukan Text'
-}
+data() {
+    return {
+    message:'Masukan Text',
+    tasks: 
+    [{
+      name:"go to shop",
+      dueDate:"2019/02/12",
+      dueTime:"18.09"
+     },
+    {
+      name:"get some fruits",
+      dueDate:"2019/02/13",
+      dueTime:"17.60"
+    }]
+    }
 },
-
 //menghapus menggunakan method
 methods:
 {
