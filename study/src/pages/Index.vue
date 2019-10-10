2b0878ca0fd4a2a2357c8c9cbd921baecc95180e
IfnuAzis <azis.ifnu@gmail.com>
2019-10-10 11:22:02 +0700

    lat

---
+++ study/src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    
+    <input v-model = "message"   @keyup = "handleKeyup"/>
+    <button @click ="clearMessage">Reset</button>
+ <H5> {{message}} </H5>
+  </q-page>
+</template>
+
+<script>
+export default {
+ 
+data(){
+  return{
+  message: 'Masukan Text'
+}
+},
+
+//menghapus menggunakan method
+methods:{
+  clearMessage(){
+    this.message=''
+  },
+
+handleKeyup(e){
+  if (e.keyCode == 27){
+    this.message = 'ESC'
+  }
+}
+}
+}
+</script>
